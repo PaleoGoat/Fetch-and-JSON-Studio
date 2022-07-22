e50353bae9036817c5a638b881f35299665f4155
Max Jewett <maximo.jewett@gmail.com>
2022-07-21 20:10:05 -0500

    Co-authored-by: pharned85 <pharned85@users.noreply.github.com> Co-authored-by: Noah <nbnhunt@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+       response.json().then(json => {
+        
+        const div = document.getElementById('container');
+        for (let i=0; i<json.length; i++)
+        div.innerHTML += `
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
+        <li>Hours in Space: ${json[i].hoursInSpace}</li>
+        <li>Active: ${json[i].active}</li>
+        <li>Skills ${json[i].skills}</li>
+        </ul>        
+        </div>
+        <img class="avatar" src=${json[i].picture}>
+        </div>
+        `
+          // Add HTML that includes the JSON data
+
+          });
+       });
+    });
+
+
+
+
+ /*
+
+           for (let i=0; i< 7; i++) {
+            astronautArr = json[i].push(id);
+            };
+
+
+            div.innerHTML = `
+            <ul>
+               <li>First Name ${json[i][j]}</li>
+            </ul>
+            `;
+
+<div class="astronaut">
+   <div class="bio">
+      <h3>Mae Jemison</h3>
+      <ul>
+         <li>Hours in space: 190</li>
+         <li>Active: false</li>
+         <li>Skills: Physician, Chemical Engineer</li>
+      </ul>
+   </div>
+   <img class="avatar" src="images/mae-jemison.jpg">
+</div>
+ */
